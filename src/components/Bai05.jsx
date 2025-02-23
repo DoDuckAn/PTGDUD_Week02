6d9dcdd9e63bbbe49c205980f23a92acc415eca2
DoDuckAn <doducan13200401@gmail.com>
2025-02-23 17:11:00 +0700

    commit Bai05 ver2

--- src/components/Bai05.jsx
+++ src/components/Bai05.jsx
@@ -11,17 +11,19 @@ function bai05(){
     }
     function handleDelete(e){
         let index=parseInt(e.target.id)
-        setTaskList(taskList.splice(index,1))
-        console.log(index)
+        let newList=[...taskList]
+        newList.splice(index,1)
+        setTaskList(newList)
     }
     return(
-        <>
+        <div style={{display:'flex',justifyContent:'top',alignItems:'center',flexDirection:'column', border:'1px solid black',width:600,minHeight:500,padding:10,backgroundColor:'lightblue'}}>
             <input onChange={handleChangeInput} type="text" placeholder="input task"></input>
             <br/>
+            <button onClick={handleAdd}>Add task</button>
             <ul>
                 {taskList.map((item,index)=>{
                     return(
-                        <li key={index}>
+                        <li key={index} style={{display:'flex',justifyContent:'space-between', alignItems:'center',width:200, borderRadius:5,backgroundColor:'white',margin:5,padding:5}}>
                             <span>{item}</span>
                             <button id={index} onClick={handleDelete}>x</button>
                         </li>
@@ -29,8 +31,8 @@ function bai05(){
                 })
                 }
             </ul>
-            <button onClick={handleAdd}>Add task</button>
-        </>
+            
+        </div>
     )
 }
 export default bai05;
